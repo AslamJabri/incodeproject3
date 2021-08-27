2ca42026910dcc2e23d5d6c1d805ebce385cefee
Aslam Jabri <jabri.aslam@gmail.com>
2021-08-27 21:33:50 +1000

    Project 3a

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express' ) 
 const data = require ('./data')
 const app = express()
-const port = 3100
+const port = 5001
+const bcrypt = require('bcrypt');
+
+
+app.use(express.json())
+app.use((express.urlencoded)({extended : false}))
 
 //console.log(message)
 //req-request  , res- response
@@ -16,21 +21,34 @@ app.get('/users/:id',(req , res) => {
     //const user = [data.user.id];
     res.send(data.users[req.params.id])
 })
-/*app.get('/users/:id', (req, res) => {
-    const { id } = req.params;
-    const userFound = users.find((user, index) => {
-        if (index == id) {
-            return user;
-        };
-    });
-    if (userFound) {
-        res.send(userFound);
-    } else {
-        res.send(`User ${id} does not exist in our database.`);
-    }
+app.post('/users',(req , res)=>{
+    //let {firstname , lastname , email , password} = req.body
+    let {firstname , lastname , email , password} = req.body
+    
+    let use = req.body
+    
+
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(password, salt);
+    
+    const newUser = {firstname,
+    lastname,
+    email,
+    password : hash }
+    
+    data.users.push(newUser)
+    res.json(data.users)
+})    
+
+
+
+
+app.get('/users/:id/schedules', (req,res) => {
+    const userPost =     data.schedules.filter(schedule => schedule.user_id === Number(req.params.id))
+
+    //console.log(userPost)
+    res.json(userPost)
 })
-*/
-        
     
 
 
