873d6f241786a7eeec35d80dba34f341589964d8
Aslam Jabri <jabri.aslam@gmail.com>
2021-09-03 00:03:50 +1000

    project 3b

--- index.js
+++ index.js
@@ -21,57 +21,81 @@ app.get('/',(req , res) => {
     res.render("pages/index")
 })
  app.get('/users',(req , res) => {
-    
-    res.render("pages/users", {users : data.users})
+    let day = date()
+    res.render("pages/users", {users : data.users , day})
 })
 app.get('/schedules',(req , res) => {
     let day = date()
    
-    res.render("pages/schedules",{schedules : data.schedules, day : day})
+    res.render("pages/schedules",{schedules : data.schedules, day})
 })
 
 //route to particular id
 app.get('/users/:id',(req , res) => {
     const { id } = req.params
-    let users = data.users 
-    const userFound = ' '
-    res.render('pages/user_id',{id : id , users:users , userFound: userFound })
+    
+    res.render('pages/user_id',{id, users:data.users , userFound: ' ' })
     
     //const user = [data.user.id];
     //res.send(data.users[req.params.id])
     //const use = (data.users[req.params.id])
     
 })
-app.post('/users',(req , res)=>{
+app.post('/usersnew', urlencodedParser , (req , res)=>{
     //let {firstname , lastname , email , password} = req.body
-    let {firstname , lastname , email , password} = req.body
+   
+    // let {firstname , lastname , email , password} = req.body
     
-    let use = req.body
+    
     
 
     const salt = bcrypt.genSaltSync(10);
     const hash = bcrypt.hashSync(password, salt);
     
-    const newUser = {firstname,
-    lastname,
-    email,
-    password : hash }
+    // //const newUser = {firstname,
+    //     lastname,
+    //    // email,
+    //     password : hash }
+    console.log(req.body)
     
-    data.users.push(newUser)
-    res.json(data.users)
+    // data.users.push(newUser)
+    // res.json(data.users)
+
+    res.render('pages/usersnew',{salt ,hash , newUser , user : data.users})
 })    
+//app.post('/usersnew',(req , res)=>{
+    //let {firstname , lastname , email , password} = req.body
+    
+//     let {firstname , lastname , email , password} = req.body
+    
+//     let use = req.body
+    
+
+//     const salt = bcrypt.genSaltSync(10);
+//     const hash = bcrypt.hashSync(password, salt);
+    
+//     const newUser = {firstname,
+//     lastname,
+//     email,
+//     password : hash }
+    
+//     data.users.push(newUser)
+//     //res.json(data.users)
+
+//     res.render('pages/user_add' , {use : use ,
+//         users: data.users , 
+//          })
+// })    
 
 
 
 
 app.get('/users/:id/schedules', (req,res) => {
-    const { id } = req.params
-    let schedules = data.schedules
-    const userPost = " "
-    let day = date()
-    //data.schedules.filter(schedule => schedule.user_id === Number(req.params.id))
+    const  id  = req.params;
+    
+    //const userPost = data.schedules.filter(schedule => schedule.user_id === Number(req.params.id))
     //console.log(userPost)
-    res.render("pages/schedules_id", { id:id ,schedules : schedules ,userPost : userPost , day : day })
+    res.render("pages/schedules_id", { id  , schedules : data.schedules , schedule : ' ', day : date() })
 })
     
 
